51591287df8b13f7ce36f3bee031fe37103f1df6
HyoungKi <lhkgogo@gmail.com>
2022-08-21 08:33:38 +0000

    내장 라이브러리

---
+++ python-basic/innerFn.py
@@ -0,0 +1,26 @@
+result = sum([1, 2, 3, 4, 5])
+print(result)
+
+print("###############################")
+
+result = min(7, 3, 5, 2)
+print(result)
+
+print("###############################")
+
+result = eval("(3 + 5) * 7")
+print(result)
+
+print("###############################")
+
+result = sorted([9, 1, 8, 5, 4])
+print(result)
+result = sorted([9, 1, 8, 5, 4], reverse 
+ = True)
+print(result)
+
+print("###############################")
+
+result = sorted([('홍길동', 35), ('이순신', 75), ('아무개', 50)], key = lambda x: x[1], reverse = True)
+print(result)
+
--- python-basic/inout.py
+++ python-basic/inout.py
@@ -1,5 +1,7 @@
+# 1
 n = int(input())
 
+# 2 3 4 5 6
 data = list(map(int, input().split()))
 
 data.sort(reverse=True)
@@ -7,6 +9,7 @@ print(data)
 
 print("###############################")
 
+# 3 4 6
 n, m, k = map(int, input().split())
 
 print(n, m, k)
@@ -15,5 +18,36 @@ print("###############################")
 
 import sys
 
+# Hello World
 data = sys.stdin.readline().rstrip()
 print(data)
+
+a = 1
+b = 2
+
+print(a, b)
+
+print("###############################")
+
+a = 1
+b = 2
+
+print(a)
+print(b)
+
+print("###############################")
+
+answer = 7
+
+print("정답은 " + str(answer) + "입니다.")
+
+print("###############################")
+
+answer = 7
+
+print("정답은", str(answer), "입니다.")
+
+print("###############################")
+
+answer = 7 
+print(f"정답은 {answer}입니다")
