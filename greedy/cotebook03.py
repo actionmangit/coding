a2cbccb44c0789015743942018f9334524fccf34
actionman <lhkgogo@gmail.com>
2022-08-22 02:16:18 +0900

    주석 입력

--- greedy/cotebook03.py
+++ greedy/cotebook03.py
@@ -1,11 +1,16 @@
+# 3 3
 n, m = map(int, input().split())
 
 result = 0
+# 3 1 2
+# 4 1 4
+# 2 2 2
 for i in range(n):
     data = list(map(int, input().split()))
     min_value = min(data)
     result = max(result, min_value)
 
+# 2
 print(result)
 
 
